d4e0f5e72944f1a4e45560ee722e3829c26d4d8e
MarcoVoss <marcovoss.wichtig@gmx.de>
2022-03-09 13:58:52 +0100

    Responsive (einigermaßen)

--- Backend-Onboarding/Webseite/js/function.js
+++ Backend-Onboarding/Webseite/js/function.js
@@ -14,3 +14,25 @@ function test(id) {
         container.style.boxShadow = 'none';
     }
 }
+
+function menuClick() {
+    var logo = document.getElementById('main-logo');
+    var image = document.getElementById('menu-img');
+    var content = document.getElementById('content');
+    var navigation = document.getElementById('app-navigation');
+    if (content.style.display != 'none') {
+        content.style.display = 'none';
+        navigation.style.display = 'flex';
+        image.src = 'img/black-menu.png';
+        logo.src = 'img/logo-reanmo-dark.png';
+        document.body.style.backgroundImage = 'linear-gradient(to bottom, white, white)';
+        document.querySelector('footer').style = 'display: none';
+    } else {
+        content.style.display = 'inline-block';
+        navigation.style.display = 'none';
+        image.src = 'img/white-menu.png';
+        logo.src = 'img/logo-reanmo.png';
+        document.body.style.backgroundImage = 'linear-gradient(to bottom, #001427, #001427), linear-gradient(to bottom, white, white)';
+        document.querySelector('footer').style = 'display: block';
+    }
+}
