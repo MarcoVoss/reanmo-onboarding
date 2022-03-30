04e09dd3588e87c605347743c7a729753d56c0e2
MarcoVoss <marcovoss.wichtig@gmx.de>
2022-03-30 09:49:24 +0200

    Made AuthTest more dynamic.

--- Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/AuthTest.php
+++ Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/AuthTest.php
@@ -11,52 +11,56 @@ class AuthTest extends TestCase
 {
     use DatabaseMigrations, RefreshDatabase;
 
+    private ?User $user = null;
+
     public function setUp(): void
     {
         parent::setUp();
         $this->seed();
+        $this->user = User::factory()->makeOne();
     }
 
-    public function test_user_registration_success() {
-        $data = [
-            'name' => 'Marco',
-            'email' => 'marco@gmx.de',
-            'password' => '#1LKNnmeE',
-            'password_confirmation' => '#1LKNnmeE'
+    private function generateUserLoginData(User $user)
+    {
+        return [
+            'email' => $user->email,
+            'password' => $user->password,
         ];
-        $response = $this->post('/api/register', $data);
+    }
+
+    private function generateUserRegisterData(User $user)
+    {
+        return [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password
+        ];
+    }
+
+
+    public function test_user_registration_success() {
+
+        $response = $this->post('/api/register', $this->generateUserRegisterData($this->user));
         $response->assertStatus(201);
     }
 
     public function test_user_registration_failure_missing_password_confirmation() {
-        $data = [
-            'name' => 'Marco',
-            'email' => 'marco@gmx.de',
-            'password' => '#1LKNnmeE',
-        ];
+        $data = $this->generateUserRegisterData($this->user);
+        unset($data['password_confirmation']);
         $response = $this->post('/api/register', $data);
         $response->assertStatus(302);
     }
 
     public function test_user_registration_failure_wrong_email() {
-        $data = [
-            'name' => 'Marco',
-            'email' => 'marco_gmx_de',
-            'password' => '#1LKNnmeE',
-            'password_confirmation' => '#1LKNnmeE',
-        ];
+        $data = $this->generateUserRegisterData($this->user);
+        $data['email'] = 'marco_gmx_de';
         $response = $this->post('/api/register', $data);
         $response->assertStatus(302);
     }
 
     public function test_user_duplicate_mail() {
-        $data = [
-            'name' => 'Marco',
-            'email' => 'test@gmx.de',
-            'image' => 'some/where',
-            'password' => '#1LKNnmeE',
-            'password_confirmation' => '#1LKNnmeE'
-        ];
+        $data = $this->generateUserRegisterData($this->user);
         $response = $this->post('/api/register', $data);
         $response->assertStatus(201);
         $response = $this->post('/api/register', $data);
@@ -65,35 +69,36 @@ class AuthTest extends TestCase
 
     public function test_login_success() {
         $this->test_user_registration_success();
-        $data = [
-            'email' => 'marco@gmx.de',
-            'password' => '#1LKNnmeE',
-        ];
+        $data = $this->generateUserLoginData($this->user);
         $response = $this->post('/api/login', $data);
         $response->assertStatus(200);
+        $this->assertIsString($response['token']);
+        $response->assertJson([
+            "user" => [
+                "name" => $this->user->name,
+                "email" => $this->user->email
+            ],
+        ]);
+        $this->assertIsInt($response["user"]["id"]);
+        $this->assertIsString($response["token"]);
     }
 
     public function test_login_wrong_credentials() {
         $user = User::find(1);
         $this->be($user);
-        $data = [
-            'email' => $user->email,
-            'password' => 'nix'
-        ];
+        $data = $this->generateUserLoginData($this->user);
+        $data['password'] = 'nix';
         $response = $this->post('/api/login', $data);
         $response->assertStatus(401);
     }
 
     public function test_not_enough_credentials() {
-        $data = [
-            'password' => 'bla',
-        ];
-        $response = $this->post('/api/login', $data);
+        $response = $this->post('/api/login');
         $response->assertStatus(302);
     }
 
     public function test_logout() {
-        $this->be(User::find(1));
+        $this->be($this->user);
         $this->post('/api/logout')->assertStatus(204);
     }
 }
