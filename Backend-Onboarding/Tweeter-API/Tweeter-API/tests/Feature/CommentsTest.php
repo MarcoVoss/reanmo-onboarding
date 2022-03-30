1bf057656aea3058cd6211453767a51f646440b1
MarcoVoss <marcovoss.wichtig@gmx.de>
2022-03-30 14:12:56 +0200

    CommentsTest refactored.

--- Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/CommentsTest.php
+++ Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/CommentsTest.php
@@ -11,119 +11,124 @@ class CommentsTest extends TestCase
 {
     use DatabaseMigrations, RefreshDatabase;
 
-    private const MY_USER_ID = 1;
-    private const NOT_EXISTING_ID = 10000000;
-    private const MY_COMMENT_ID = 1;
-    private const OTHER_COMMENT_ID = 20;
-    private const EXISTING_POST_ID = 1;
-
     public function setUp(): void
     {
         parent::setUp();
         $this->seed();
     }
 
-    public function test_store_success()
+    public function test_CanDestroyOwnComment()
     {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'post_id' => self::EXISTING_POST_ID,
+        $user = User::factory()->create();
+        $comment = $user->posts()->create([
+            "user_id" => $user->id,
+            "message" => "Test"
+        ])->comments()->create([
+            "user_id" => $user->id,
+            "message" => "Test"
+        ]);
+        $this->be($user);
+        $this->delete("/api/comments/".$comment->id)
+            ->assertStatus(204);
+    }
+
+    public function test_CantDestroyOtherUsersComment()
+    {
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+        $comment = $otherUser->posts()->create([
+            "user_id" => $otherUser->id,
+            "message" => "Test"
+        ])->comments()->create([
+            "user_id" => $otherUser->id,
+            "message" => "Test"
+        ]);
+        $this->be($user);
+        $this->delete("/api/comments/".$comment->id)
+            ->assertStatus(403);
+    }
+
+    public function test_CanCreateCommentOnExistingPost()
+    {
+        $user = User::factory()->create();
+        $post = $user->posts()->create([
+            "message" => "Test"
+        ]);
+
+        $this->be($user);
+        $this->post('/api/comments', [
+            'post_id' => $post->id,
             'message' => 'Test',
-        ];
-        $this->post('/api/comments', $data)
+        ])->assertCreated()
+            ->assertJsonStructure([
+                "message",
+                "user"
+            ])
+            ->assertJsonPath("likes", 0)
             ->assertStatus(201);
     }
 
-    public function test_store_failure_post_does_not_exist()
+    public function test_CantStoreWithMissingBody()
     {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'post_id' => self::NOT_EXISTING_ID,
-            'message' => 'Test',
-        ];
-        $this->post('/api/comments', $data)
+        $this->be(User::factory()->create());
+        $this->post('/api/comments')
             ->assertStatus(302);
     }
 
-    public function test_store_failure_missing_message()
+    public function test_CanUpdateComment()
     {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'post_id' => self::EXISTING_POST_ID,
-        ];
-        $this->post('/api/comments', $data)
-            ->assertStatus(302);
-    }
-
-    public function test_store_failure_no_data_presented()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [];
-        $this->post('/api/comments', $data)
-            ->assertStatus(302);
-    }
-
-    public function test_update_success()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'message' => 'Test',
-        ];
-        $this->put('/api/comments/'.self::MY_COMMENT_ID, $data)
-            ->assertStatus(200);
-    }
-
-    public function test_update_failure_missing_message()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'post_id' => self::EXISTING_POST_ID,
-        ];
-        $this->put('/api/comments/'.self::MY_COMMENT_ID, $data)
-            ->assertStatus(302);
-    }
-
-    public function test_update_failure_wrong_comment_id()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'message' => 'Test',
-        ];
-        $this->put("/api/comments/".self::NOT_EXISTING_ID, $data)
-            ->assertStatus(404);
-    }
-
-    public function test_update_failure_unauthorized()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'message' => 'Test',
-        ];
-        $this->put("/api/comments/".self::OTHER_COMMENT_ID, $data)
-            ->assertStatus(403);
-    }
-
-    public function test_destroy_failure_wrong_id()
-    {
-        $user = User::find(self::MY_USER_ID);
+        $user = User::factory()->create();
+        $comment = $user->posts()->create([
+            "user_id" => $user->id,
+            "message" => "Test"
+        ])->comments()->create([
+            "user_id" => $user->id,
+            "message" => "Test"
+        ]);
         $this->be($user);
-        $this->delete("/api/comments/".self::NOT_EXISTING_ID)
-            ->assertStatus(404);
+        $data = [
+            'message' => 'Test',
+        ];
+        $this->put('/api/comments/'.$comment->id, $data)
+            ->assertOk()
+            ->assertJsonStructure([
+                "message",
+                "user"
+            ])
+            ->assertJsonPath("likes", $comment->likes->count());
     }
 
-    public function test_destroy_failure_unauthenticated()
+    public function test_CantUpdateWithMissingBody()
     {
-        $user = User::find(self::MY_USER_ID);
+        $user = User::factory()->create();
+        $comment = $user->posts()->create([
+            "user_id" => $user->id,
+            "message" => "Test"
+        ])->comments()->create([
+            "user_id" => $user->id,
+            "message" => "Test"
+        ]);
         $this->be($user);
-        $this->delete("/api/comments/".self::OTHER_COMMENT_ID)
-            ->assertStatus(403);
+        $this->put('/api/comments/'.$comment->id)
+            ->assertStatus(302);
     }
 
-    public function test_destroy_success()
+    public function test_CantUpdateOthersPost()
     {
-        $user = User::find(self::MY_USER_ID);
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+        $comment = $otherUser->posts()->create([
+            "user_id" => $otherUser->id,
+            "message" => "Test"
+        ])->comments()->create([
+            "user_id" => $otherUser->id,
+            "message" => "Test"
+        ]);
         $this->be($user);
-        $this->delete("/api/comments/".self::MY_COMMENT_ID)
-            ->assertStatus(204);
+        $data = [
+            'message' => 'Test',
+        ];
+        $this->put('/api/comments/'.$comment->id, $data)
+            ->assertForbidden();
     }
 }
