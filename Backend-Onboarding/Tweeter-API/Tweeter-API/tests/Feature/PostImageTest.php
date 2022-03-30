503d8d4d66bc02051fac880287fdad3dfb6dd2e0
MarcoVoss <marcovoss.wichtig@gmx.de>
2022-03-30 14:58:34 +0200

    Refactored PostImageTest.

--- Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/PostImageTest.php
+++ Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/PostImageTest.php
@@ -12,74 +12,38 @@ class PostImageTest extends TestCase
 {
     use DatabaseMigrations, RefreshDatabase;
 
-    private const MY_USER_ID = 1;
-    private const OTHER_USER_ID = 2;
-    private const NOT_EXISTING_ID = 100000;
-
-    private const MY_POST_ID = 1;
-    private const OTHER_POST_ID = 11;
-
     public function setUp(): void
     {
         parent::setUp();
         $this->seed();
     }
 
-    public function test_update_success()
+    public function test_CanUploadImage()
     {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
+        $user = User::factory()->create();
+        $post = $user->posts()->create([
+            "message" => "Test"
+        ]);
+        $this->be($user);
+        $this->put('/api/posts/'.$post->id.'/image', [
             'image' => UploadedFile::fake()->image('avatar.jpg'),
-        ];
-        $this->put('/api/posts/'.self::MY_POST_ID.'/image', $data)
-            ->assertStatus(201);
-    }
-
-    public function test_update_failure_wrong_user()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
-            'image' => UploadedFile::fake()->image('avatar.jpg'),
-        ];
-        $this->put('/api/posts/'.self::NOT_EXISTING_ID.'/image', $data)
-            ->assertStatus(404);
-    }
-
-    public function test_update_success_no_image()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $this->put('/api/posts/'.self::MY_POST_ID.'/image')
+        ])->assertCreated();
+        $this->put('/api/posts/'.$post->id.'/image')
             ->assertStatus(204);
     }
 
-    public function test_update_failure_unauthorized()
+    public function test_CanNotUpdateOthersImage()
     {
-        $this->be(User::find(self::MY_USER_ID));
-        $data = [
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+        $post = $otherUser->posts()->create([
+            "message" => "Test"
+        ]);
+        $this->be($user);
+        $this->put('/api/posts/'.$post->id.'/image', [
             'image' => UploadedFile::fake()->image('avatar.jpg'),
-        ];
-        $this->put('/api/posts/'.self::OTHER_POST_ID.'/image', $data)
+        ])->assertStatus(403);
+        $this->put('/api/posts/'.$post->id.'/image')
             ->assertStatus(403);
-    }
-
-    public function test_destroy_failure_non_existing_post()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $this->put('/api/posts/'.self::NOT_EXISTING_ID.'/image')
-            ->assertStatus(404);
-    }
-
-    public function test_destroy_failure_unauthenticated()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $this->put('/api/posts/'.self::OTHER_POST_ID.'/image')
-            ->assertStatus(403);
-    }
-
-    public function test_destroy_success()
-    {
-        $this->be(User::find(self::MY_USER_ID));
-        $this->put('/api/posts/'.self::MY_POST_ID.'/image')
-            ->assertStatus(204);
     }
 }
