0b6b3a1101be677f1361639bc355d9be33ac3eda
MarcoVoss <marcovoss.wichtig@gmx.de>
2022-03-30 14:48:21 +0200

    Deleted trash.

--- Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/PostCommentTest.php
+++ Backend-Onboarding/Tweeter-API/Tweeter-API/tests/Feature/PostCommentTest.php
@@ -11,16 +11,12 @@ class PostCommentTest extends TestCase
 {
     use DatabaseMigrations, RefreshDatabase;
 
-    private const MY_USER_ID = 1;
-    private const NOT_EXISTING_ID = 100000;
-
     public function setUp(): void
     {
         parent::setUp();
         $this->seed();
     }
-
-
+    
     public function test_CanReadCommentsByPost()
     {
         $user = User::factory()->create();
